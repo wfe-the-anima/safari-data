6796e9fa0d29ca7946e5311878f37b10255608d8
wfe-the-anima <garyoakhart@gmail.com>
2015-12-01 18:04:03 +0000

    Create BotOne.js

---
+++ System/Bots/BotOne.js
@@ -0,0 +1,18 @@
+var Bot;
+
+function(Bot) {
+    if (Bot == "undefined") {
+        print("undefined");
+    Bot: "SafariBot",
+  return true;
+  }
+}
+
+//Because Of TOEN
+
+exports.toen = [
+      print(SafariBot);
+      when (toen.add == "Bot") {
+          push.commit(Safaribot)@newToenVars
+          }
+      ]
